104f0d2eba161e797bb4d2c70cf4651baa85e947
Kern <825761175@qq.com>
2020-03-12 21:18:14 +0800

    poibox

--- tools/poibox/src/main/java/cn/kerninventor/tools/poibox/testdemo/TestStyle.java
+++ tools/poibox/src/main/java/cn/kerninventor/tools/poibox/testdemo/TestStyle.java
@@ -2,10 +2,15 @@ package cn.kerninventor.tools.poibox.testdemo;
 
 import cn.kerninventor.tools.poibox.BoxBracket;
 import cn.kerninventor.tools.poibox.BoxGadget;
+import cn.kerninventor.tools.poibox.elements.StylerElements;
+import cn.kerninventor.tools.poibox.style.Styler;
 import cn.kerninventor.tools.poibox.style.TabulationStyle;
 import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.FillPatternType;
+
+import javax.swing.text.Style;
 
 /**
  * @Title: TestStyle
@@ -19,15 +24,20 @@ public class TestStyle implements TabulationStyle {
 
     @Override
     public CellStyle getHeadLineStyle() {
-        CellStyle cellStyle = BoxGadget.root().styler().usualHeadLine(null);
+        Styler styler = BoxGadget.root().styler();
+        CellStyle cellStyle = styler.usualHeadLine(null);
         cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-        cellStyle.setFillForegroundColor(HSSFColor.HSSFColorPredefined.LIGHT_BLUE.getIndex());
+        styler.setBorder(StylerElements.CellDirection.SURROUND, BorderStyle.DOUBLE);
+        cellStyle.setFillForegroundColor(HSSFColor.HSSFColorPredefined.LIGHT_CORNFLOWER_BLUE.getIndex());
         return cellStyle;
     }
 
     @Override
     public CellStyle getTableHeadStyle() {
-        return BoxGadget.root().styler().usualTableHeader(null);
+        CellStyle cellStyle = BoxGadget.root().styler().usualTableHeader(null);
+        cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        cellStyle.setFillForegroundColor(HSSFColor.HSSFColorPredefined.GREY_25_PERCENT.getIndex());
+        return cellStyle;
     }
 
     @Override
