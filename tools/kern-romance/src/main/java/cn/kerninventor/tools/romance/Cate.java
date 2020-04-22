9d61af15a575844287f7709e90e9795392fefa4e
Kern <825761175@qq.com>
2020-04-22 17:29:36 +0800

    romance

---
+++ tools/kern-romance/src/main/java/cn/kerninventor/tools/romance/Cate.java
@@ -0,0 +1,12 @@
+package cn.kerninventor.tools.romance;
+
+/**
+ * @author Kern
+ * @date 2020/4/22 17:25
+ * @description
+ */
+public interface Cate {
+
+    String FLAVOUR = "It's very delicious!";
+
+}
