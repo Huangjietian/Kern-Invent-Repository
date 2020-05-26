4940a782c84205f542f5442e6b53a66221e7a1e9
Kern <825761175@qq.com>
2020-05-26 17:23:31 +0800

    poibox

--- tools/poibox-opensource/src/main/java/cn/kerninventor/tools/poibox/opensource/data/tabulation/writer/tbody/TableBodyDataWriter.java
+++ tools/poibox-opensource/src/main/java/cn/kerninventor/tools/poibox/opensource/data/tabulation/writer/tbody/TableBodyDataWriter.java
@@ -3,14 +3,20 @@ package cn.kerninventor.tools.poibox.opensource.data.tabulation.writer.tbody;
 import cn.kerninventor.tools.poibox.opensource.BoxGadget;
 import cn.kerninventor.tools.poibox.opensource.data.tabulation.context.ColumnDefinition;
 import cn.kerninventor.tools.poibox.opensource.data.tabulation.context.TableContext;
+import cn.kerninventor.tools.poibox.opensource.data.tabulation.statistics.NumberStatisticsLogicHandler;
+import cn.kerninventor.tools.poibox.opensource.data.tabulation.statistics.NumberStatisticsMode;
 import cn.kerninventor.tools.poibox.opensource.data.tabulation.writer.ETabulationWriter;
 import cn.kerninventor.tools.poibox.opensource.data.tabulation.writer.tbody.col.ColWriter;
+import cn.kerninventor.tools.poibox.opensource.utils.BeanUtil;
 import cn.kerninventor.tools.poibox.opensource.utils.ReflectUtil;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Kern
@@ -51,7 +57,37 @@ public class TableBodyDataWriter<T> implements TbodyWriter<T> {
         colWriter.flush();
     }
 
-    private void preNumberStatistics(String fieldName, Number number) {
+    private void preNumberStatistics() {
+        //大致思路， 在获得字段值后 收集到 数组中
+    }
 
+    private void writeNumberStatistics() {
+        //大致思路， 对收集的结果进行汇总计算。
+    }
+    private class StatisticsTask {
+
+        private Map<String, List<Number>> columnsNumberValue;
+        private NumberStatisticsLogicHandler handler;
+        private NumberStatisticsMode mode;
+
+        public StatisticsTask(String[] fieldsName, NumberStatisticsMode mode) {
+            columnsNumberValue = new HashMap<>(fieldsName.length);
+            for (String fieldName : fieldsName) {
+                columnsNumberValue.put(fieldName, new ArrayList<>(16));
+            }
+            handler = new NumberStatisticsLogicHandler();
+        }
+
+        public void collect(String fieldName, Number number) {
+            columnsNumberValue.get(fieldName).add(number);
+        }
+
+        public Number doStatistics(String fieldName){
+            List<Number> numberList = columnsNumberValue.get(fieldName);
+            if (BeanUtil.isEmpty(numberList)) {
+                return 0;
+            }
+            return handler.toStatistics(mode, numberList.toArray(new Number[numberList.size()]));
+        }
     }
 }
