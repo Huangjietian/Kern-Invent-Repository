305ae6592548b47b11ec0f59b34556ee5541985c
Kern <825761175@qq.com>
2020-06-17 12:15:49 +0800

    Tree类优化

--- tools/common-utils/src/main/java/cn/kerninventory/tools/common/structure/Tree.java
+++ tools/common-utils/src/main/java/cn/kerninventory/tools/common/structure/Tree.java
@@ -1,6 +1,7 @@
 package cn.kerninventory.tools.common.structure;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
@@ -11,11 +12,13 @@ import java.util.stream.Collectors;
  * @author Kern
  * @version 1.0
  */
-public class Tree<T extends Sapling> {
+public class Tree<T extends Sapling, R> {
 
     private T trunk;
 
-    private List<Tree<T>> branches;
+    private R result;
+
+    private List<Tree<T, R>> branches;
 
     private Tree(T trunk) {
         this.trunk = trunk;
@@ -34,7 +37,7 @@ public class Tree<T extends Sapling> {
      * @param trunk
      * @return
      */
-    public Tree<T> setTrunk(T trunk) {
+    public Tree<T, R> setTrunk(T trunk) {
         this.trunk = trunk;
         return this;
     }
@@ -43,7 +46,7 @@ public class Tree<T extends Sapling> {
      * 获取当前结构的分支
      * @return
      */
-    public List<Tree<T>> getBranches() {
+    public List<Tree<T, R>> getBranches() {
         return branches;
     }
 
@@ -52,9 +55,42 @@ public class Tree<T extends Sapling> {
      * @param branches
      * @return
      */
-    public Tree<T> setBranches(List<Tree<T>> branches) {
+    public Tree<T, R> setBranches(List<Tree<T, R>> branches) {
         this.branches = branches;
         return this;
+    }
+
+    public R getResult() {
+        return result;
+    }
+
+    public Tree<T, R> setResult(R result) {
+        this.result = result;
+        return this;
+    }
+
+    /**
+     * <p>
+     *     对树状结构当前层及其下一层次进行遍历操作，使树形结构自当前层起得到一系列的结果。需要指定一个Function函数
+     * </p>
+     * @param function
+     */
+    public void forEachOperate(Function<T, R> function) {
+        operate(function);
+        List<Tree<T, R>> branches = getBranches();
+        if (branches != null) {
+            branches.forEach(e -> e.forEachOperate(function));
+        }
+    }
+
+    /**
+     * <p>
+     *     对树形结构的当前层进行操作，得到一个Result结果。需要指定一个Function函数
+     * </p>
+     * @param function
+     */
+    public void operate(Function<T ,R> function) {
+        setResult(function.apply(getTrunk()));
     }
 
     /**
@@ -65,9 +101,10 @@ public class Tree<T extends Sapling> {
      * @param collection
      * @param rootStrategy
      * @param <T>
+     * @param <R>
      * @return
      */
-    public static <T extends Sapling> Tree<T> of(Collection<T> collection, RootStrategy rootStrategy) {
+    public static <T extends Sapling, R> Tree<T, Object> of(Collection<T> collection, RootStrategy rootStrategy) {
         T root = findRoot(collection, rootStrategy);
         return of(collection, root);
     }
@@ -80,16 +117,17 @@ public class Tree<T extends Sapling> {
      * @param collection
      * @param root
      * @param <T>
+     * @param <R>
      * @return
      */
-    public static <T extends Sapling> Tree<T> of(Collection<T> collection, Sapling root) {
+    public static <T extends Sapling, R> Tree<T, Object> of(Collection<T> collection, Sapling root) {
         if (root == null) {
             root = findRoot(collection, e -> e.rootNode() == null);
         }
         if (root == null) {
             throw new IllegalArgumentException("No root was found!");
         }
-        Tree<T> tree = new Tree(root);
+        Tree<T, Object> tree = new Tree(root);
         tree.decorate(collection);
         return tree;
     }
@@ -102,13 +140,11 @@ public class Tree<T extends Sapling> {
                 Tree tree = new Tree(t);
                 tree.decorate(collection);
                 addBranches(tree);
-                //FIXME 下面的代码将导致  ConcurrentModificationException
-//                iterator.remove();
             }
         }
     }
 
-    private void addBranches(Tree<T> tree) {
+    private void addBranches(Tree<T, R> tree) {
         if (branches == null) {
             branches = new ArrayList<>();
         }
