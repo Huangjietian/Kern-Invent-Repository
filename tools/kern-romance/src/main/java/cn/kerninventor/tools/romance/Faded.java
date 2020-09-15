33b170b6bd1d6a8aec219e6240e9a1fcc006dbdc
Kern <825761175@qq.com>
2020-09-15 10:01:43 +0800

    Faded Riddle

---
+++ tools/kern-romance/src/main/java/cn/kerninventor/tools/romance/Faded.java
@@ -0,0 +1,11 @@
+package cn.kerninventor.tools.romance;
+
+/**
+ * <p>
+ *     Something gone.
+ * </p>
+ *
+ * @author Kern
+ */
+public interface Faded {
+}
---
+++ tools/kern-romance/src/main/java/cn/kerninventor/tools/romance/Riddle.java
@@ -0,0 +1,24 @@
+package cn.kerninventor.tools.romance;
+
+/**
+ * <p>
+ *                          ?   ?   ?
+ *                       ?            ?
+ *                     ?               ?
+ *                                      ?
+ *                                     ?
+ *                                   ?
+ *                               ?
+ *                             ?
+ *                             ?
+ *                             ?
+ *
+ *                             ?
+ * </p>
+ *
+ * @author Kern
+ */
+public interface Riddle {
+
+    Object guess();
+}
